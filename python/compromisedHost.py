d8ac756c7efcb2d5d97fd65622783bb7a005c1d9
Thilina Ratnayake <tratnayake@my.bcit.ca>
2015-09-25 16:50:43 -0700

    CH

--- python/compromisedHost.py
+++ python/compromisedHost.py
@@ -10,9 +10,34 @@ def checkPackets(packet):
     IP = packet[1];
     TCP = packet[2]
 
-
-    if IP.src =="192.168.0.18":
+    if IP.ttl == TTLkey:
         print "PACKET!"
+        print IP.show()
+        print "IP identification is " + str(IP.id)
+        print "S Port is " + str(TCP.sport)
+
+        #Convert to binary
+        print IP.id
+        IPbin = bin(IP.id)
+        print IPbin
+        seg1 = IPbin[2:10]
+        seg2 = IPbin[10:21]
+        print seg1
+        print seg2
+
+        print TCP.sport
+        SPbin = bin(TCP.sport)
+        print SPbin
+        # seg3 = SPbin[2:10]
+        # seg4 = SPbin[10:21]
+        # print seg3
+        # print seg4
+
+
+
+
+
+
 
 
 
@@ -21,8 +46,9 @@ def checkPackets(packet):
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 
+
 global TTLkey
-TTLkey = sys.argv[1]
+TTLkey = int(sys.argv[1])
 
 #1. Always be listening for that control packet
 sniff(filter="ip",prn=checkPackets)
